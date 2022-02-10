ecd93d5e2eaaaa0257a40b6fe371d88907bf145c
merziy <alexzandermathis@gmail.com>
2022-02-10 16:07:48 -0500

    Finished alien_invasion

--- alien_invasion/README.md
+++ alien_invasion/README.md
@@ -1,3 +1,3 @@
 # Alien Invasion game
 
-This is a game I wrote from following chapters 12 and 13.
+This is a game I wrote from following chapters 12-14 of Matthes's Python Crash Course book. This program helped me understand a lot of background logic, patterns, compartmentalization, the need for refractoring, and introduced me to Python a great deal. It also exposed me to the pygame library and general game development.
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings"""
 
     def __init__(self):
-        """Initializes games' settings """
+        """Initializes game's static settings """
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -13,14 +13,38 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = .50
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        # How quickly the alien point value increases
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """initialize settings that change throughout the game"""
+        self.ship_limit = 3
+        self.bullet_speed = 3.0
+        self.alien_speed = .5
+
+        # fleect_directiom of 1 represents right; -1 is left
         self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings and point values"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
